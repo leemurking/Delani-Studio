be28ce0c0f2ac7f11d2325b3779105d18494fda7
leemurking <85164123+leemurking@users.noreply.github.com>
2021-06-21 13:02:27 +0300

    Link JavaScript

---
+++ myscripts.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    $("#img1").click(function () {
+        $("#p1").toggle();
+        $("#img1").toggle();
+    });
+    $("#p1").click(function () {
+        $("#p1").toggle();
+        $("#img1").toggle();
+    });
+
+    $("#img2").click(function () {
+        $("#p2").toggle();
+        $("#img2").toggle();
+    });
+    $("#p2").click(function () {
+        $("#p2").toggle();
+        $("#img2").toggle();
+    });
+
+    $("#img3").click(function () {
+        $("#p3").toggle();
+        $("#img3").toggle();
+    });
+    $("#p3").click(function () {
+        $("#p3").toggle();
+        $("#img3").toggle();
+    });  
+      
+})
+
+
