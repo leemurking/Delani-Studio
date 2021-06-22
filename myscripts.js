a1b6fbf2178edf4f9738e48af878ca628317a319
leemurking <85164123+leemurking@users.noreply.github.com>
2021-06-22 09:21:16 +0300

    Add Border color to hover

--- myscripts.js
+++ myscripts.js
@@ -24,8 +24,81 @@ $(document).ready(function () {
     $("#p3").click(function () {
         $("#p3").toggle();
         $("#img3").toggle();
-    });  
-      
+    }); 
+    
+    $("#div1").hover(
+        function () {
+          $(this).addClass("overlay");
+          $(".hoover2").show();
+        },
+        function () {
+          $(".hoover2").hide();
+        }
+      );
+      $("#div3").hover(
+        function () {
+          $(this).addClass("overlay");
+          $(".hoover3").show();
+        },
+        function () {
+          $(".hoover3").hide();
+        }
+      );
+      $("#div4").hover(
+        function () {
+          $(this).addClass("overlay");
+          $(".hoover4").show();
+        },
+        function () {
+          $(".hoover4").hide();
+        }
+      );
+      $("#div5").hover(
+        function () {
+          $(this).addClass("overlay");
+          $(".hoover5").show();
+        },
+        function () {
+          $(".hoover5").hide();
+        }
+      );
+      $("#div6").hover(
+        function () {
+          $(this).addClass("overlay");
+          $(".hoover6").show();
+        },
+        function () {
+          $(".hoover6").hide();
+        }
+      );
+      $("#div7").hover(
+        function () {
+          $(this).addClass("overlay");
+          $(".hoover7").show();
+        },
+        function () {
+          $(".hoover7").hide();
+        }
+      );
+      $("#div8").hover(
+        function () {
+          $(this).addClass("overlay");
+          $(".hoover8").show();
+        },
+        function () {
+          $(".hoover8").hide();
+        }
+      );
+      $("#div9").hover(
+        function () {
+          $(this).addClass("overlay");
+          $(".hoover9").show();
+        },
+        function () {
+          $(".hoover9").hide();
+        }
+      );
+
 })
 
 
